19a1c4e899aaaa076413543a6771df820e06cbe3
admin <etamayo@birt.eus>
2021-05-28 20:19:29 +0200

    ghhg

---
+++ InterfazGrafica/UC/ListaPedidosUC.cs
@@ -0,0 +1,31 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicLayer;
+
+namespace InterfazGrafica.UC
+{
+    public partial class ListaPedidosUC : UserControl
+    {
+        private List<PedidoDTO> listaPedidos;
+
+        public ListaPedidosUC(String nombreCliente)
+        {
+            InitializeComponent();
+            // TODO: haz la peticion de los clientes y asociala a la lista y cargalo en el DataGridView
+            this.dGV_Pedidos.DataSource = ControladorBLL.ListarPedidosCliente(nombreCliente);
+         
+        }
+
+        public List<PedidoDTO> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+
+
+    }
+}
